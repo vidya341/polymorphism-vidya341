050d1cd0ffdbe8a8e944cce9fdb5a914a796f572
vidya <vidyashivakumar13@gmail.com>
2023-07-13 19:22:58 +0530

    changes made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Main {
+    //product class
+    Product p = new Product();
+    int a = p.product(5,10);
+    int b = p.product(5,10,5);
+    double c = p.product(5.2,10.2);
+
 
 }
